23dfe853fb29645dfc3c2f12c5fd036f12871575
Yulinarsw <113989816+Yulinarsw@users.noreply.github.com>
2022-09-25 20:01:46 +0700

    Add files via upload

---
+++ JavaProgram.java
@@ -0,0 +1,10 @@
+
+import javax.swing.*;
+public class JavaProgram {
+    public static void main(String[]args){
+       
+        //No 2
+        String InputKata = JOptionPane.showInputDialog("Anda Sengan belajar apa?");
+        JOptionPane.showMessageDialog(null, "Belajar " + InputKata + " sangat menyenangkan ");
+    }
+}
---
+++ OutputMonitor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Yulinar sri wahyuni
+ */
+public class OutputMonitor {
+    public static void main (String[]args){
+      
+    System.out.println("Belajar JAVA");
+    System.out.println("Sangat mudah sekali");
+    System.out.println("dan sangat menyenangkan");
+    }
+}
+      
