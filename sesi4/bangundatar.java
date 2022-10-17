a72a51d3d16ad084783ed3df897d17b86ff38fd3
Yulinarsw <113989816+Yulinarsw@users.noreply.github.com>
2022-10-17 20:44:25 +0700

    Add files via upload

---
+++ sesi4/bangundatar.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BangunDatar;
+
+
+public class bangundatar {
+    float luas(){
+        System.out.print("Menghitung luas bangun datar");
+        return 0;
+    }
+    
+    float keliling(){
+        System.out.print("Menghitung keliling bangun datar");
+        return 0;
+    }
+}
