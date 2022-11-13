aee26c0d903973366c76d2cb1a9da840f82eeac4
Yulinarsw <113989816+Yulinarsw@users.noreply.github.com>
2022-11-13 19:51:22 +0700

    Add files via upload

---
+++ sesi6/BelajarPackage/Kendaraan.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alat.transportasi.beroda;
+
+import com.alat.transportasi.beroda.KendaraanBeroda;
+public class Kendaraan {
+    public static void main(String[] args){
+        System.out.println("Belajar package dan interface");
+    }
+}
